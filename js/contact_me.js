f9208816ca7865731c2cd423f1629d74a7fd2bd6
cedrickbrandao <cedrickbrandao@gmail.com>
2016-02-18 14:24:08 -0200

    Correcao email

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,13 +18,14 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://177.1.199.224:8080/riafone/upload/arquivos",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+					acao: "email"
                 },
                 cache: false,
                 success: function() {
